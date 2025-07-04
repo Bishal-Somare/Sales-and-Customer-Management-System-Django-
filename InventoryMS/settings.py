84d1f7d63955405aa3a5d9a6ee3ed84c513974d8
Bishal-Somare-Magar <149509990+Bishal-Somare@users.noreply.github.com>
2025-07-04 07:05:32 +0545

    added * in the allowed host

--- InventoryMS/settings.py
+++ InventoryMS/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g_n2+2bznu6e@1wel!i(&-4tp86_7lop5395ww+i4x%9*7^old'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'daphne',
